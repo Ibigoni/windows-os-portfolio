b7327e6dd4b1c314725ea9191df0442d219490d3
Ibigoni Inye-Tam <inyetamibigoni@gmail.com>
2026-01-06 20:05:50 -0600

    window manager to render applications

---
+++ src/components/WindowManager.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Window from "./Window";
+import { useWindowsStore } from "../state/windowsStore";
+import AboutApp from "../apps/AboutApp";
+import ProjectApps from "../apps/ProjectsApp";
+import ResumeApp from "../apps/ResumeApp";
+
+/**
+ * Renders a collection of application windows based on the windows state from the store.
+ * Each window displays a different application component depending on the `app` property:
+ * - "about" renders the AboutApp component
+ * - "projects" renders the ProjectApps component
+ * - "resume" renders the ResumeApp component
+ * 
+ * @component
+ * @returns {JSX.Element} A fragment containing all active windows with their respective applications
+ */
+export default function WindowManager() {
+  const windows = useWindowsStore((s) => s.windows);
+
+  return (
+    <>
+      {windows.map((w) => {
+        <Window key={w.id} win={w}>
+          {w.app === "about" && <AboutApp />}
+          {w.app === "projects" && <ProjectApps />}
+          {w.app === "resume" && <ResumeApp />}
+        </Window>
+      })}
+    </>
+  );
+}
