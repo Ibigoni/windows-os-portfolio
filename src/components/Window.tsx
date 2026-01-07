22c10da3570dad71f606f2873b917dc68334d093
Ibigoni Inye-Tam <inyetamibigoni@gmail.com>
2026-01-06 19:30:45 -0600

    window function to handle windown component

---
+++ src/components/Window.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import Draggable, { DraggableEvent, DraggableData } from "react-draggable";
+import { useWindowsStore, WindowState } from "../state/windowsStore";
+
+
+export default function Window({ win, children }: { win: WindowState; children: React.ReactNode }) {
+  const focusWindow = useWindowsStore((s) => s.focusWindow);
+  const closeWindow = useWindowsStore((s) => s.closeWindow);
+  const toggleMinimize = useWindowsStore((s) => s.toggleMinimize);
+  const toggleMaximize = useWindowsStore((s) => s.toggleMaximize);
+  const moveWindow = useWindowsStore((s) => s.moveWindow);
+
+
+  // minimized windows are "running" but not visible
+  if (win.isMinimized) return null;
+
+  const baseStyle: React.CSSProperties = win.isMaximized 
+  ? { top: 0, left: 0, width: "100w", height: "calc(100vh - 48px" }
+  : { width: win.size.w, height: win.size.h };
+
+  const content = (
+    <div 
+      className="absolute rounded-xl overflow-hidden border border-white/10 bg-zinc-900/90 backdrop-blur text-white shadow-2xl"
+      style={{ ...baseStyle, zIndex: win.zIndex }}
+      onMouseDown={() => focusWindow(win.id)}
+    >
+      <div className="h-10 flex items-center justify-between px-3 bg-zinc-950/70 border-b border-white/10 select-none cursor-move">
+        <div className="text-sm font-medium">{win.title}</div>
+
+        <div className="flex gap-2">
+
+          {/* minimize */}
+          <button
+            className="w-8 h-7 rounded hover:bg-white/10"
+            onClick={(e) => {
+              e.stopPropagation();
+              toggleMinimize(win.id);
+            }}
+          >
+            &#128469;
+          </button>
+
+          {/* maximize */}
+          <button
+            className="w-8 h-7 rounded hover:bg-white/10"
+            onClick={(e) => {
+              e.stopPropagation();
+              toggleMaximize(win.id);
+            }}
+          >
+            &#128470;
+          </button>
+
+          {/* close */}
+          <button
+            className="w-8 h-7 rounded hover:bg-red-500/70"
+            onClick={(e) => {
+              e.stopPropagation();
+              toggleMaximize(win.id);
+            }}  
+          >
+            &#128473;
+          </button>
+
+        </div>
+      </div>
+
+      <div className="h-[calc(100%-40px)] p-4 overflow-auto">{children}</div>
+    </div>
+  );
+
+  if (win.isMaximized) return content;
+
+  return (
+    <Draggable
+      handle=".cursor-move"
+      defaultPosition={win.position}
+      onStop={(_: DraggableEvent, data: DraggableData) => moveWindow(win.id, data.x, data.y)}
+    >
+      {content}
+    </Draggable>
+  );
+
+}
